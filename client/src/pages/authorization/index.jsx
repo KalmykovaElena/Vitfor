5bcb42f23daf6c58fc7e5c286110f9d31e24f76e
KalmykovaElena <kalmykhovae@gmail.com>
2023-07-10 11:10:29 +0300

    change logo

--- client/src/pages/authorization/index.jsx
+++ client/src/pages/authorization/index.jsx
@@ -4,12 +4,13 @@ import background from 'assets/bg-auth.png';
 import Logo from 'components/logo';
 import AuthPanel from 'components/authPanel';
 import Select from 'components/common/select';
+import logo from 'assets/logo.png';
 
 export default function Authorization() {
   return (
     <>
       <header className="authorization-header">
-        <Logo />
+        <Logo name="app" img={logo} text="VitFor" textLocation="right" />
         <Select data={['РУС', 'EN', 'BY']} onchangeSelect={(e) => console.log(e)} />
       </header>
       <main className="authorization" style={{ backgroundImage: `url(${background})` }}>
