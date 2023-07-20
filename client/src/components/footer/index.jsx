a99f988ce62fd65d0de1bc3e689c84cd212cf375
KalmykovaElena <kalmykhovae@gmail.com>
2023-07-20 14:28:49 +0300

    fixed footer styles and onClick function

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { footerData } from 'constants/footerData';
 import './index.scss';
 import { useSelector } from 'react-redux';
 
 const Footer = () => {
+  const navigate = useNavigate();
   const theme = useSelector((state) => state.auth.theme);
   return (
     <footer className={`footer footer_${theme}`}>
       {footerData.map((e) => (
-        <div className="footer-item" key={e.id}>
+        <div className="footer-item" key={e.id} onClick={() => navigate(e.link)}>
           <img src={e.img} alt="logo" />
-          <NavLink to={e.link}> {e.name}</NavLink>
+          <span>{e.name}</span>
         </div>
       ))}
     </footer>
