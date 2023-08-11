765659af53c4097bb2aa3b90393638b90a705020
Shtukar Artem <shtukar95@mail.ru>
2023-08-11 14:47:18 +0300

    added ChatHeader component

---
+++ client/src/components/ChatHeader/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
+import styles from './index.module.scss';
+import { UserIcon } from '../UserIcon';
+
+export const ChatHeader = ({ img, userName, userNickName }) => {
+  const theme = useSelector((state) => state.auth.theme);
+  return (
+    <div className={styles.wrapper}>
+      <UserIcon img={img} userName={userName} />
+      <div className={styles.names}>
+        <span className={classNames(styles.userName, { [styles.lightUserNameTheme]: theme === 'light' })}>
+          {userName}
+        </span>
+        <span className={classNames(styles.userNickname, { [styles.lightUserNickname]: theme === 'light' })}>
+          {userNickName}
+        </span>
+      </div>
+      {/* TODO 3 точки с dropdownmenu */}
+    </div>
+  );
+};
