99e22803d42264ab5207dd64c0f6959ed8ca2910
KalmykovaElena <kalmykhovae@gmail.com>
2023-08-17 12:06:07 +0300

    changed import styles sceleton

--- client/src/components/common/CardSceleton/index.jsx
+++ client/src/components/common/CardSceleton/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
@@ -9,24 +9,22 @@ import style from './index.module.scss';
 const CardSceleton = () => {
   const theme = useSelector((state) => state.auth.theme);
   return (
-    <SkeletonTheme color="#979393" highlightColor="#ede7e7">
-      <section className={classNames(style.wrapper, { [style.light]: theme === 'light' })}>
-        <div className={style.display}>
-          <Skeleton height="12rem" width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
+    <section className={classNames(style.wrapper, { [style.light]: theme === 'light' })}>
+      <div className={style.display}>
+        <Skeleton height="12rem" width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
+      </div>
+      <h4 className={style.title}>
+        <div className={style.left}>
+          <Skeleton height={30.35} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
         </div>
-        <h4 className={style.title}>
-          <div className={style.left}>
-            <Skeleton height={30.35} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
-          </div>
-          <div className={style.right}>
-            <Skeleton height={30.35} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
-          </div>
-        </h4>
-        <p className={style.row}>
-          <Skeleton height={17.99} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
-        </p>
-      </section>
-    </SkeletonTheme>
+        <div className={style.right}>
+          <Skeleton height={30.35} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
+        </div>
+      </h4>
+      <p className={style.row}>
+        <Skeleton height={17.99} width="100%" borderRadius="1.25rem" duration={3} baseColor="#bdb4b44a" />
+      </p>
+    </section>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,18 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { SkeletonTheme } from 'react-loading-skeleton';
 import App from './App';
 import store from './redux/store';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <SkeletonTheme color="#979393" highlightColor="#ede7e7">
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </SkeletonTheme>
 );
